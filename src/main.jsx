ca43223dbd9cebe98697d7b2866a77c5a7b4712b
Yurii <yurii.n@saasjet.com>
2025-09-30 11:23:01 +0300

    delete strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
